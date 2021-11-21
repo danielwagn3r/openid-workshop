9c73e5daecdeaab8d5d355b88d8dc30f4c70fa44
Daniel Wagner <daniel@wagners.name>
2021-11-21 02:28:54 +0100

    Refactored node demo api.
    Added logging middleware
    Fixed requires

--- Apis/NodeCalculatorApi/server.js
+++ Apis/NodeCalculatorApi/server.js
@@ -6,7 +6,27 @@ const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 const jwtAuthz = require('express-jwt-authz');
 const jsonwebtoken = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+
+const winston = require('winston')
+const consoleTransport = new winston.transports.Console()
+const myWinstonOptions = {
+  transports: [consoleTransport]
+}
+
+const logger = new winston.createLogger(myWinstonOptions)
+
+function logRequest(req, res, next) {
+  logger.info(req.url)
+  next()
+}
+app.use(logRequest)
+
+function logError(err, req, res, next) {
+  logger.error(err)
+  next()
+}
+app.use(logError)
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
