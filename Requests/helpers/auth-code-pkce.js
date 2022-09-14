b66d207783a47bd0e6b5f17cd3beecb0a55ad0a8
Daniel Wagner <daniel@wagners.name>
2022-09-14 23:09:51 +0200

    Fixed PKCE handling
    Generation of code_verifyer and code_challange adapted

--- Requests/helpers/auth-code-pkce.js
+++ Requests/helpers/auth-code-pkce.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const base64url = require('base64url');
 
 
-var code_verifier = base64url.encode(crypto.randomBytes(32));
+var code_verifier = randomString();
 
 console.log(`code verifier: ${code_verifier}`);
 
@@ -10,3 +10,8 @@ var hash = crypto.createHash('sha256').update(code_verifier).digest();
 var code_challenge = base64url.encode(hash)
 
 console.log(`code challenge: ${code_challenge}`);
+
+/** @returns {string} crypto-generated base64 string */
+function randomString() {
+    return base64url.encode(crypto.randomBytes(32))
+}
