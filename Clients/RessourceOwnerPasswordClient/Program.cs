c518d40055df55933019690ca949c5bc64ce4f7e
Daniel Wagner <daniel@wagners.name>
2022-09-16 19:14:16 +0200

    Reformated code

--- Clients/RessourceOwnerPasswordClient/Program.cs
+++ Clients/RessourceOwnerPasswordClient/Program.cs
@@ -1,144 +1,142 @@
-﻿using IdentityModel.Client;
-using Microsoft.Extensions.Configuration;
-using Serilog;
-using Serilog.Sinks.SystemConsole.Themes;
-using System;
+﻿using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
-namespace RessourceOwnerPasswordClient
+namespace RessourceOwnerPasswordClient;
+
+public class Program
 {
-    internal class Program
+    private static IConfiguration _configuration;
+
+    private static async Task Main(string[] args)
     {
-        private static IConfiguration _configuration;
+        var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{environment}.json", true, true)
+            .AddEnvironmentVariables();
 
-        private static async Task Main(string[] args)
+        _configuration = builder.Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .ReadFrom.Configuration(_configuration)
+            .CreateLogger();
+
+        var domain = $"https://{_configuration["Auth0:Domain"]}/";
+
+        Log.Debug("Domain: {domain}", domain);
+
+        var disco = await GetDiscoveryResponse(domain);
+
+        var tokenResponse = await GetTokenResponse(disco);
+
+        var refreshToken = tokenResponse.RefreshToken;
+
+        await CallApi(tokenResponse.AccessToken);
+
+        tokenResponse = await RefreshTokenResponse(disco, refreshToken);
+
+        Log.CloseAndFlush();
+    }
+
+    private static async Task<DiscoveryDocumentResponse> GetDiscoveryResponse(string domain)
+    {
+        // discover endpoints from metadata
+        var cache = new DiscoveryCache(domain);
+
+        var disco = await cache.GetAsync();
+        if (disco.IsError)
         {
-            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
-
-            _configuration = builder.Build();
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .ReadFrom.Configuration(_configuration)
-                .CreateLogger();
-
-            string domain = $"https://{_configuration["Auth0:Domain"]}/";
-
-            Log.Debug("Domain: {domain}", domain);
-
-            var disco = await GetDiscoveryResponse(domain);
-
-            var tokenResponse = await GetTokenResponse(disco);
-
-            string refreshToken = tokenResponse.RefreshToken;
-
-            await CallApi(tokenResponse.AccessToken);
-
-            tokenResponse = await RefreshTokenResponse(disco, refreshToken);
-
-            Log.CloseAndFlush();
+            Log.Error("Discovery error: {Error}", disco.Error);
+            throw new Exception(disco.Error);
         }
 
-        private static async Task<DiscoveryDocumentResponse> GetDiscoveryResponse(string domain)
-        {
-            // discover endpoints from metadata
-            var cache = new DiscoveryCache(domain);
+        Log.Information("Issuer: {Issuer}", disco.Issuer);
 
-            var disco = await cache.GetAsync();
-            if (disco.IsError)
+        return disco;
+    }
+
+    private static async Task<TokenResponse> GetTokenResponse(DiscoveryDocumentResponse disco)
+    {
+        var client = new HttpClient();
+
+        // request token
+        var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+        {
+            Address = disco.TokenEndpoint,
+
+            ClientId = _configuration["Auth0:ClientId"],
+            ClientSecret = _configuration["Auth0:ClientSecret"],
+
+            UserName = _configuration["Auth0:Username"],
+            Password = _configuration["Auth0:Password"],
+
+            Scope = "openid email profile offline_access calc:double",
+
+            Parameters =
             {
-                Log.Error("Discovery error: {Error}", disco.Error);
-                throw new Exception(disco.Error);
+                {"audience", _configuration["CalculatorApi:ApiIdentifier"]}
             }
+        });
 
-            Log.Information("Issuer: {Issuer}", disco.Issuer);
-
-            return disco;
-        }
-
-        private static async Task<TokenResponse> GetTokenResponse(DiscoveryDocumentResponse disco)
+        if (response.IsError)
         {
-            var client = new HttpClient();
-
-            // request token
-            var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest()
-            {
-                Address = disco.TokenEndpoint,
-
-                ClientId = _configuration["Auth0:ClientId"],
-                ClientSecret = _configuration["Auth0:ClientSecret"],
-
-                UserName = _configuration["Auth0:Username"],
-                Password = _configuration["Auth0:Password"],
-
-                Scope = "openid email profile offline_access calc:double",
-
-                Parameters =
-                {
-                    {"audience", _configuration["CalculatorApi:ApiIdentifier"]}
-                }
-            });
-
-            if (response.IsError)
-            {
-                Log.Error("Token error: {Error}\n{Description}", response.Error, response.ErrorDescription);
-                throw new Exception(response.Error);
-            }
-
-            Log.Information("Access Token: {AccessToken}", response.AccessToken);
-            Log.Information("Identity Token: {IdentityToken}", response.IdentityToken);
-            Log.Information("Refresh Token: {RefreshToken}", response.RefreshToken);
-
-            return response;
+            Log.Error("Token error: {Error}\n{Description}", response.Error, response.ErrorDescription);
+            throw new Exception(response.Error);
         }
 
-        private static async Task<TokenResponse> RefreshTokenResponse(DiscoveryDocumentResponse disco, string refreshToken)
+        Log.Information("Access Token: {AccessToken}", response.AccessToken);
+        Log.Information("Identity Token: {IdentityToken}", response.IdentityToken);
+        Log.Information("Refresh Token: {RefreshToken}", response.RefreshToken);
+
+        return response;
+    }
+
+    private static async Task<TokenResponse> RefreshTokenResponse(DiscoveryDocumentResponse disco, string refreshToken)
+    {
+        var client = new HttpClient();
+
+        // request token
+        var response = await client.RequestRefreshTokenAsync(new RefreshTokenRequest
         {
-            var client = new HttpClient();
+            Address = disco.TokenEndpoint,
 
-            // request token
-            var response = await client.RequestRefreshTokenAsync(new RefreshTokenRequest()
-            {
-                Address = disco.TokenEndpoint,
+            ClientId = _configuration["Auth0:ClientId"],
+            ClientSecret = _configuration["Auth0:ClientSecret"],
 
-                ClientId = _configuration["Auth0:ClientId"],
-                ClientSecret = _configuration["Auth0:ClientSecret"],
+            RefreshToken = refreshToken
+        });
 
-                RefreshToken = refreshToken
-            });
-
-            if (response.IsError)
-            {
-                Log.Error("Token error: {Error}\n{Description}", response.Error, response.ErrorDescription);
-                throw new Exception(response.Error);
-            }
-
-            Log.Information("Access Token: {AccessToken}", response.AccessToken);
-            Log.Information("Identity Token: {IdentityToken}", response.IdentityToken);
-            Log.Information("Refresh Token: {RefreshToken}", response.RefreshToken);
-
-            return response;
-        }
-
-        private static async Task CallApi(string token)
+        if (response.IsError)
         {
-            var client = new HttpClient()
-            {
-                BaseAddress = new Uri(_configuration["CalculatorApi:Address"])
-            };
-
-            client.SetBearerToken(token);
-
-            var response = await client.GetStringAsync("/api/double/3");
-
-            Log.Information("Response {response}", response);
+            Log.Error("Token error: {Error}\n{Description}", response.Error, response.ErrorDescription);
+            throw new Exception(response.Error);
         }
+
+        Log.Information("Access Token: {AccessToken}", response.AccessToken);
+        Log.Information("Identity Token: {IdentityToken}", response.IdentityToken);
+        Log.Information("Refresh Token: {RefreshToken}", response.RefreshToken);
+
+        return response;
+    }
+
+    private static async Task CallApi(string token)
+    {
+        var client = new HttpClient
+        {
+            BaseAddress = new Uri(_configuration["CalculatorApi:Address"])
+        };
+
+        client.SetBearerToken(token);
+
+        var response = await client.GetStringAsync("/api/double/3");
+
+        Log.Information("Response {response}", response);
     }
 }
